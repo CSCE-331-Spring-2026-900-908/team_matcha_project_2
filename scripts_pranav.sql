076823664780948ab427055522db1077e91f40db
Pranav Kalaiselvan <pranavkalai04@gmail.com>
2026-02-17 23:18:23 -0600

    Added query for avg cost of order for each employee

--- scripts_pranav.sql
+++ scripts_pranav.sql
@@ -8,3 +8,10 @@ ORDER BY total_revenue DESC;
 
 SELECT AVG(costtotal) AS avg_order_cost
 FROM orders;
+
+SELECT 
+    employeeid,
+    AVG(costtotal) AS avg_order_cost
+FROM orders
+GROUP BY employeeid
+ORDER BY avg_order_cost DESC;
